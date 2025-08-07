be76cd2449b660fbbfd265812a49eb5da220af36
daronus <isdjfv@gmail.com>
2025-08-07 15:46:28 +0900

    Feat: 로그인/회원 기능 추가

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+# users/urls.py
+from django.urls import path
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
