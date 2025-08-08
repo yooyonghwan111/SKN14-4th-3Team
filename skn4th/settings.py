f2c25cbef17f901cfb3f6ed0f0543ef27a440449
rwr9857 <rwr9857@gmail.com>
2025-08-08 11:51:33 +0900

    Chore: 환경설정 기본값 추가

--- skn4th/settings.py
+++ skn4th/settings.py
@@ -5,12 +5,12 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default="dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", cast=bool)
+DEBUG = config("DEBUG", cast=bool, default=True)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost,0.0.0.0").split(",")
 
 
 # Application definition
